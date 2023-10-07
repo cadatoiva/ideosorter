ab11ff051497cae0b389ce297f8cc8d3bf183826
cadatoiva <2086652+cadatoiva@users.noreply.github.com>
2023-10-06 20:08:42 -0500

    Move folder and delete branch

--- public_html/scripts/page-index.js
+++ public_html/scripts/page-index.js
@@ -21,11 +21,9 @@ i18n.$l10nLangs.forEach(lang => {
 const textBody = document.getElementById("text_body")
 
 textBody.innerHTML = [...i18n.ui_index_text].join("")
-document.getElementById("title").innerText = i18n.ui_index_title
-document.getElementById("startbutton").innerText = i18n.ui_index_start
-document.getElementById("treebutton").innerText = i18n.ui_index_tree
+document.getElementById("title").innerHTML = i18n.ui_index_title
+document.getElementById("startbutton").innerHTML = i18n.ui_index_start
 document.getElementById("contacts").innerHTML = [...i18n.ui_index_contacts].join("")
 document.getElementById("contacts_info").innerHTML = [...i18n.ui_index_contact_info].join("")
 document.getElementById("startbutton").onclick = () => location.href = `quiz.html?${i18n.$lang}`
-document.getElementById("treebutton").onclick = () => location.href = `tree.html?${i18n.$lang}`
-document.getElementById("ideo_length").innerText = results.length.toFixed(0)
+document.getElementById("ideo_length").innerHTML = results.length
