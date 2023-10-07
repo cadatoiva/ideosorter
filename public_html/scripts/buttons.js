62874315957d4b941fc042bb564545030e555987
cadatoiva <2086652+cadatoiva@users.noreply.github.com>
2023-10-07 11:05:01 -0500

    Rearrange buttons alphabetically

--- public_html/scripts/buttons.js
+++ public_html/scripts/buttons.js
@@ -1,32 +1,17 @@
 export default {
-    yes: {
-        text: i18n.btn_yes,
-        bgcolor: "#14e371",
-        textcolor: "#000"
-    },
-    no: {
-        text: i18n.btn_no,
-        bgcolor: "#e3144f",
-        textcolor: "#000"
-    },
-    nostate: {
-        text: i18n.btn_nostate,
-        bgcolor: "#111",
-        textcolor: "#fff"
-    },
-    conditional: {
-        text: i18n.btn_conditional,
-        bgcolor: "#50e024",
-        textcolor: "#000"
-    },
-    unconditional: {
-        text: i18n.btn_unconditional,
-        bgcolor: "#f0cb11",
-        textcolor: "#000"
-    },
     capitalism: {
         text: i18n.btn_capitalism,
         bgcolor: "#f0cb11",
+        textcolor: "#000"
+    },
+    classnation: {
+        text: i18n.btn_classnation,
+        bgcolor: "#821212",
+        textcolor: "#fff"
+    }
+    conditional: {
+        text: i18n.btn_conditional,
+        bgcolor: "#50e024",
         textcolor: "#000"
     },
     corporatism: {
@@ -34,34 +19,19 @@ export default {
         bgcolor: "#704d23",
         textcolor: "#fff"
     },
-    workers: {
-        text: i18n.btn_workers,
-        bgcolor: "#e3144f",
-        textcolor: "#000"
-    },
-    unions: {
-        text: i18n.btn_unions,
-        bgcolor: "#e3144f",
-        textcolor: "#000"
-    },
-    state: {
-        text: i18n.btn_state,
-        bgcolor: "#2462ad",
-        textcolor: "#000"
+    dasein: {
+        text: i18n.btn_dasein,
+        bgcolor: "#111",
+        textcolor: "#fff"
     },
     electoralism: {
         text: i18n.btn_electoralism,
         bgcolor: "#c47112",
         textcolor: "#000"
     },
-    revolution: {
-        text: i18n.btn_revolution,
-        bgcolor: "#f0cb11",
-        textcolor: "#000"
-    },
-    welfare: {
-        text: i18n.btn_welfare,
-        bgcolor: "#f0cb11",
+    no: {
+        text: i18n.btn_no,
+        bgcolor: "#e3144f",
         textcolor: "#000"
     },
     noneed: {
@@ -69,14 +39,44 @@ export default {
         bgcolor: "#50e024",
         textcolor: "#000"
     },
-    dasein: {
-        text: i18n.btn_dasein,
+    nostate: {
+        text: i18n.btn_nostate,
         bgcolor: "#111",
         textcolor: "#fff"
     },
-    classnation: {
-        text: i18n.btn_classnation,
-        bgcolor: "#821212",
-        textcolor: "#fff"
-    }
+    revolution: {
+        text: i18n.btn_revolution,
+        bgcolor: "#f0cb11",
+        textcolor: "#000"
+    },
+    state: {
+        text: i18n.btn_state,
+        bgcolor: "#2462ad",
+        textcolor: "#000"
+    },
+    unconditional: {
+        text: i18n.btn_unconditional,
+        bgcolor: "#f0cb11",
+        textcolor: "#000"
+    },
+    unions: {
+        text: i18n.btn_unions,
+        bgcolor: "#e3144f",
+        textcolor: "#000"
+    },
+    workers: {
+        text: i18n.btn_workers,
+        bgcolor: "#e3144f",
+        textcolor: "#000"
+    },
+    welfare: {
+        text: i18n.btn_welfare,
+        bgcolor: "#f0cb11",
+        textcolor: "#000"
+    },
+    yes: {
+        text: i18n.btn_yes,
+        bgcolor: "#14e371",
+        textcolor: "#000"
+    },
 }
